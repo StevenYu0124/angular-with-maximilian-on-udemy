cdcea6ee651f415368d52cd353421ed4d3309a08
Steven Yu <stevenyu@Stevens-MacBook-Air.local>
2025-11-26 00:14:11 +0800

    update

--- 10. A New Starting Project & Analyzing The Project Structure/01-starting-project/src/app/app.component.ts
+++ 10. A New Starting Project & Analyzing The Project Structure/01-starting-project/src/app/app.component.ts
@@ -3,18 +3,20 @@ import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
 import { DUMMY_USERS } from './user/dummy-users';
 import { User } from './user/user.model';
+import { TasksComponent } from "./tasks/tasks.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, UserComponent],
+  imports: [HeaderComponent, UserComponent, TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   users = DUMMY_USERS;
+  user = DUMMY_USERS[0];
 
   onUserSelected(user: User) {
-    console.log(user);
+    this.user = user;
   }
 }
