abea601f83bca92fb33f29e8846ad91f7b00bc38
Steven Yu <stevenyu@Stevens-MacBook-Air.local>
2025-12-08 22:00:27 +0800

    apply local storage

--- 10. A New Starting Project & Analyzing The Project Structure/01-starting-project/src/app/tasks/tasks.service.ts
+++ 10. A New Starting Project & Analyzing The Project Structure/01-starting-project/src/app/tasks/tasks.service.ts
@@ -6,8 +6,15 @@ import { Injectable, signal } from '@angular/core';
 @Injectable({ providedIn: 'root' })
 export class TasksService {
     private readonly _tasks = signal<Task[]>(dummyTasks);
-
     readonly tasks = this._tasks.asReadonly();
+    constructor() {
+        const tasksString = localStorage.getItem('tasks');
+        if (tasksString) {
+            this._tasks.set(JSON.parse(tasksString));
+        } else {
+            this.save();
+        }
+    }
 
     add(newTask: NewTask): Task {
         const task: Task = {
@@ -15,10 +22,16 @@ export class TasksService {
             id: Math.random().toString(),
         };
         this._tasks.update(tasks => tasks.concat(task));
+        this.save();
         return task;
     }
 
     complete(taskId: string): void {
         this._tasks.update(tasks => tasks.filter(task => task.id !== taskId));
+        this.save();
+    }
+
+    private save() {
+        localStorage.setItem('tasks', JSON.stringify(this.tasks()));
     }
 }
